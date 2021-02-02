964dc58efa8406290af7a3c041460044e9cc934b
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2021-02-02 23:51:08 +0200

    fix server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.use(express.static("public"));
+app.use(express.static("dist"));
 app.listen(PORT);
