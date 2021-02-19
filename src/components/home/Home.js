6ab1a6dc6a9fab49a1237683bff343e551d9024d
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2021-02-19 13:11:43 +0200

    111

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,7 +5,7 @@ import MainContainer from "../includes/mainContainer/MainContainer";
 export default function Home() {
   return (
     <MainContainer>
-      <Typography variant="h3">Home test-branch</Typography>
+      <Typography variant="h3">Home test-branch 1</Typography>
     </MainContainer>
   );
 }
