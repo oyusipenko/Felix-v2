00baf179d8cac600eefe0e76f85fa196d74cd3ad
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2021-02-19 13:09:18 +0200

    tb-commit1

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,7 +5,7 @@ import MainContainer from "../includes/mainContainer/MainContainer";
 export default function Home() {
   return (
     <MainContainer>
-      <Typography variant="h3">Home</Typography>
+      <Typography variant="h3">Home test-branch</Typography>
     </MainContainer>
   );
 }
